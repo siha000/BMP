7863565ea4743d8a1f138e03ea19c37ba078edca
siha000 <applegreen7841@gmail.com>
2022-09-14 22:29:14 +0800

    1

--- TEXT.c
+++ TEXT.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include "TEXT.h"
 
 int main()
---
+++ gray/main.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<srdlib.h>
+int main()
+{
+    return 0;
+}
