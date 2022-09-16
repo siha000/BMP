d84c499663431ce7f8a4dda27eb198c21913db57
siha000 <applegreen7841@gmail.com>
2022-09-16 17:13:29 +0800

    1

--- LOW/LOW.c
+++ LOW/LOW.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <../TEXT.h>
-#define HEIGHT 1500
-#define WEIGHT 2000
-
+#include "../TEXT.h"
+#define HEIGHT 256
+#define WEIGHT 256
+unsigned char RGB[HEIGHT][WEIGHT * 3];
+unsigned char result[HEIGHT * 2][WEIGHT * 3 * 2];
+unsigned char record[HEIGHT * 2][WEIGHT * 3 * 2];
 int main()
 {
-    FILE *read = fopen("car.bmp", "rb");
-    FILE *write = fopen("car1.bmp", "wb");
-    header header;
-    DIBHEADER DIBHEADER;
-    fread(&header, sizeof(header), 1, read);
-    fread(&DIBHEADER, sizeof(DIBHEADER), 1, read);
-    char *RGB = (char *)malloc(sizoef(char) * HEIGHT * WEIGHT * 3);
-    char *result = (char *)malloc(sizeof(char) * WEIGHT * HEIGHT * 3);
-    char *record = (char *)calloc((HEIGHT + 2)(WEIGHT * 3 + 6),sizeof(char));
-
-    fread(RGB, sizoef(char), HEIGHT * WEIGHT * 3, read);
-    for(int c=WEIGHT*3+2;c<HEIGHT*WEIGHT*3;c++)
+    FILE *read = fopen("colorbird.bmp", "rb");
+    FILE *write = fopen("colorbird1.bmp", "wb");
+    header HEADER;
+    DIBHEADER dibheader;
+    fread(&HEADER, sizeof(HEADER), 1, read);
+    fread(&dibheader, sizeof(dibheader), 1, read);
+    fread(RGB, sizeof(unsigned char), HEIGHT * WEIGHT * 3, read);
+    for (int c = 0; c < HEIGHT * 2; c += 2)
     {
-        
+        for (int d = 0; d < WEIGHT * 3 * 2; d += 6)
+        {
+            record[c][d] = RGB[c][d];
+            record[c][d + 1] = RGB[c][d + 1];
+            record[c][d + 2] = RGB[c][d + 2];
+        }
     }
+
+    for (int c = 2; c < HEIGHT - 1; c += 2)
+    {
+        for (int d = 2; d < (WEIGHT - 1) * 2 * 3; d += 2)
+        {
+            record[c - 1][d] = (record[c][d] + record[c - 2][d]) / 2;             //[1][2]
+            record[c - 2][d - 1] = (record[c - 2][d - 2] + record[c - 2][d]) / 2; //[0][1]
+            record[c][d - 1] = (record[c][d] + record[c][d - 2]) / 2;             //[2][1]
+            record[c - 1][d - 2] = (record[c - 2][d - 2] + record[c][d - 2]) / 2; //[1][0]
+            record[c - 1][d - 1] = (record[c - 1][d] + record[c - 1][d - 2]) / 2;
+        }
+    }
+    fwrite(&HEADER, sizeof(HEADER), 1, write);
+    fwrite(&dibheader, sizeof(dibheader), 1, write);
+    fwrite(RGB, sizeof(unsigned char), HEIGHT * WEIGHT * 3, write);
+    fclose(read);
+    fclose(write);
     return 0;
 }
--- TEXT.c
+++ TEXT.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "TEXT.h"
-#define HEIGHT 300
-#define WEIGHT 250
+#define HEIGHT 256
+#define WEIGHT 256
 
-char **RGB;
+
 // int data=(24*WEIGHT+31)/32*4;
-// char RGB[HEIGHT][WEIGHT * 3 + 2];
-/*char result[HEIGHT][WEIGHT * 3 + 2];
-char record[HEIGHT + 2][WEIGHT * 3 + 6] = {0};
-*/
+char RGB[HEIGHT][WEIGHT * 3];
+char result[HEIGHT][WEIGHT * 3];
+char record[HEIGHT][WEIGHT * 3] = {0};
+
 int main()
 {
-	FILE *fp = fopen("bird.bmp", "rb");
-	FILE *write = fopen("bird1.bmp", "wb");
+	FILE *fp = fopen("colorbird.bmp", "rb");
+	FILE *write = fopen("colorbird1.bmp", "wb");
 	header text;
 	DIBHEADER dibheader;
 	fread(&text, sizeof(text), 1, fp);
 	fread(&dibheader, sizeof(dibheader), 1, fp);
-	RGB = malloc(sizeof(char *) * HEIGHT);
-	for (int d = 0; d < HEIGHT; d++)
-	{
-		RGB[d] = malloc(sizeof(char) * HEIGHT * 3 + 2);
-	}
-	char result[HEIGHT][WEIGHT * 3 + 2];
-	char record[HEIGHT + 2][WEIGHT * 3 + 6] = {0};
-	fread(&RGB, sizeof(char), HEIGHT * (WEIGHT)*3 + 2 * 300, fp);
+	fread(&RGB, sizeof(char), HEIGHT * (WEIGHT)*3, fp);
 	for (int c = 1; c < HEIGHT; c++)
 	{
 		for (int d = 3; d < WEIGHT * 3; d++)
@@ -49,7 +42,7 @@ int main()
 
 	fwrite(&text, sizeof(text), 1, write);
 	fwrite(&dibheader, sizeof(dibheader), 1, write);
-	fwrite(RGB, sizeof(char), 250 * 300 * 3 + 2 * 300, write);
+	fwrite(result, sizeof(char), HEIGHT*WEIGHT*3, write);
 	fclose(fp);
 	fclose(write);
 	free(RGB);
