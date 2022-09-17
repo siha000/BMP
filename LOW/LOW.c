9f18d0d9e7b7dd33740506f1668c91397e7290b1
siha <applegreen7841@gmail.com>
2022-09-17 13:11:32 +0800

    1

--- LOW/LOW.c
+++ LOW/LOW.c
@@ -1,34 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../TEXT.h"
-#define HEIGHT 256
-#define WEIGHT 256
-unsigned char RGB[HEIGHT][WEIGHT * 3];
-unsigned char result[HEIGHT * 2][WEIGHT * 3 * 2];
-unsigned char record[HEIGHT * 2][WEIGHT * 3 * 2];
+//#include "LOW.h"
+#define HEIGHT 316
+#define WIDTH 316
+#pragma pack(1)
+char RGB[HEIGHT * 2][WIDTH * 3];
+char record[HEIGHT * 2][WIDTH * 3 * 2] = {0};
+char result[HEIGHT * 2][WIDTH * 3 * 2] = {0};
+typedef struct BITMAP_header
+{
+    char name[2];
+    unsigned int size;
+    int garbage;
+    unsigned int image_offset;
+} header;
+
+typedef struct DIB_HEADER
+{
+    unsigned int header_size;
+    unsigned int width;
+    unsigned int height;
+    unsigned short int colorplanes;
+    unsigned short int bitsperpixel;
+    unsigned int compression;
+    unsigned int image_size;
+    unsigned int biXPelsPerMeter;
+    unsigned int biYPelsPerMeter;
+    unsigned int biClrUsed;
+    unsigned int biClrImportant;
+} DIBHEADER;
+
 int main()
 {
-    FILE *read = fopen("colorbird.bmp", "rb");
-    FILE *write = fopen("colorbird1.bmp", "wb");
+    FILE *read = fopen("Lenna.bmp", "rb");
+    FILE *write = fopen("Lenna1.bmp", "wb");
     header HEADER, HEADER1;
     DIBHEADER dibheader, dibheader1;
     fread(&HEADER, sizeof(HEADER), 1, read);
     fread(&dibheader, sizeof(dibheader), 1, read);
-    fread(RGB, sizeof(unsigned char), HEIGHT * WEIGHT * 3, read);
+    fread(RGB, sizeof(unsigned char), HEIGHT * WIDTH * 3, read);
     printf("%d %d \n", dibheader.height, dibheader.width);
-    for (int c = 0; c < HEIGHT * 2; c += 2)
+    for (int c = 0; c < HEIGHT * 2 - 2; c += 2)
     {
-        for (int d = 0; d < WEIGHT * 3 * 2; d += 6)
+        for (int d = 0; d < WIDTH * 3 * 2 - 6; d += 6)
         {
-            record[c][d] = RGB[c / 2][d / 6];
-            record[c][d + 1] = RGB[c / 2][d / 6 + 1];
-            record[c][d + 2] = RGB[c / 2][d / 6 + 2];
+            record[c][d] = RGB[c / 2][d / 2];
+            record[c][d + 1] = RGB[c / 2][d / 2 + 1];
+            record[c][d + 2] = RGB[c / 2][d / 2 + 2];
+            /*result[c][d] = record[c][d];
+            result[c][d + 1] = record[c][d];
+            result[c][d + 2] = record[c][d + 2];
+        */
         }
     }
-
+    /*for (int c = 0; c < WIDTH * 3 * 2; c += 6)
+    {
+        record[HEIGHT * 2][c] = RGB[HEIGHT][c / 2];
+        record[HEIGHT * 2][c + 1] = RGB[HEIGHT][c / 2 + 1];
+        record[HEIGHT * 2][c + 2] = RGB[HEIGHT][c / 2 + 2];
+    }
+    for (int c = 0; c < HEIGHT * 2; c += 2)
+    {
+        record[c][HEIGHT * 2 * 3] = RGB[c / 2][HEIGHT * 3];
+    }
     for (int c = 2; c < HEIGHT - 1; c += 2)
     {
-        for (int d = 2; d < (WEIGHT - 1) * 2 * 3; d += 2)
+        for (int d = 2; d < (WIDTH - 1) * 2 * 3; d += 2)
         {
             record[c - 1][d] = (record[c][d] + record[c - 2][d]) / 2;             //[1][2]
             record[c - 2][d - 1] = (record[c - 2][d - 2] + record[c - 2][d]) / 2; //[0][1]
@@ -37,15 +74,14 @@ int main()
             record[c - 1][d - 1] = (record[c - 1][d] + record[c - 1][d - 2]) / 2;
         }
     }
-
+*/
     dibheader.height = HEIGHT * 2;
-    dibheader.width = WEIGHT * 2;
+    dibheader.width = WIDTH * 2;
+    HEADER.size = HEIGHT * WIDTH * 3 * 2 * 2;
     fwrite(&HEADER, sizeof(HEADER), 1, write);
     fwrite(&dibheader, sizeof(dibheader), 1, write);
-    fwrite(record, sizeof(unsigned char), HEIGHT * WEIGHT * 3 * 2 * 2, write);
+    fwrite(record, sizeof(unsigned char), HEIGHT * WIDTH * 3 * 2 * 2, write);
 
-    fread(&HEADER1, sizeof(HEADER1), 1, write);
-    fread(&dibheader1, sizeof(dibheader1), 1, write);
     printf("%d %d \n", dibheader.height, dibheader.width);
     fclose(read);
     fclose(write);
---
+++ LOW/LOW.h
@@ -0,0 +1,53 @@
+#define HEIGHT 256
+#define WIDTH 256
+#define file_name "colorbird.bmp"
+#define file_save "colorbird1.bmp"
+char RGB[HEIGHT][WIDTH * 3];
+char record[HEIGHT + 2][WIDTH * 3 + 6];
+char result[HEIGHT][WIDTH * 3];
+#pragma pack(1)
+
+typedef struct BITMAP_header
+{
+	char name[2];
+	unsigned int size;
+	int garbage;
+	unsigned int image_offset;
+} header;
+
+typedef struct DIB_HEADER
+{
+	unsigned int header_size;
+	unsigned int width;
+	unsigned int height;
+	unsigned short int colorplanes;
+	unsigned short int bitsperpixel;
+	unsigned int compression;
+	unsigned int image_size;
+	unsigned int biXPelsPerMeter;
+	unsigned int biYPelsPerMeter;
+	unsigned int biClrUsed;
+	unsigned int biClrImportant;
+} DIBHEADER;
+
+void open()
+{
+	FILE *read = fopen(file_name, "rb");
+	header header;
+	DIBHEADER DIBHEADER;
+	fread(&header, sizeof(header), 1, read);
+	fread(&DIBHEADER, sizeof(DIBHEADER), 1, read);
+	fread(RGB, sizeof(char), HEIGHT * WIDTH * 3, read);
+	fclose(read);
+}
+
+void save_image()
+{
+	FILE *write = fopen(file_save, "wb");
+	header header1;
+	DIBHEADER DIBHEADER1;
+	fwrite(&header1, sizeof(header1), 1, write);
+	fwrite(&DIBHEADER1, sizeof(DIBHEADER1), 1, write);
+	fwrite(result, sizeof(char), HEIGHT * WIDTH * 3, write);
+	fclose(write);
+}
